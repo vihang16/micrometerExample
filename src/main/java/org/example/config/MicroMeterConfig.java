6defd8edc1c76600473758c34f7ce4fc24932a7b
Vihang Shah <vihang.shah@tesco.com>
2023-06-09 13:43:08 +0200

    feat: updating observation factory

--- src/main/java/org/example/config/MicroMeterConfig.java
+++ src/main/java/org/example/config/MicroMeterConfig.java
@@ -1,30 +1,78 @@
 package org.example.config;
 
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Metrics;
+import io.micrometer.observation.Observation;
 import io.micrometer.observation.ObservationRegistry;
-import io.micrometer.observation.aop.ObservedAspect;
-import org.slf4j.MDC;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.reactive.server.ReactiveWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.observation.DefaultServerRequestObservationConvention;
+import org.springframework.http.server.observation.ServerRequestObservationConvention;
+import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.http.server.reactive.observation.ServerHttpObservationDocumentation;
+import org.springframework.http.server.reactive.observation.ServerRequestObservationContext;
+import reactor.core.publisher.Mono;
 import reactor.util.Logger;
 import reactor.util.Loggers;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 
 @Configuration(proxyBeanMethods = false)
 public class MicroMeterConfig {
 
     private static final Logger logger = Loggers.getLogger(MicroMeterConfig.class);
 
-    @Bean
-    public ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
-        return new ObservedAspect(observationRegistry);
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    WebServerFactoryCustomizer<ReactiveWebServerFactory> observedTomcatWebServerFactoryCustomizer(
+            ObservationRegistry observationRegistry) {
+        return factory -> factory.getWebServer(new ObservedValve(observationRegistry));
+
+    }
+}
+class ObservedValve implements HttpHandler {
+    private static final ServerRequestObservationConvention DEFAULT_OBSERVATION_CONVENTION = new DefaultServerRequestObservationConvention();
+    private final ObservationRegistry observationRegistry;
+
+
+    public ObservedValve(ObservationRegistry observationRegistry) {
+        this.observationRegistry =  observationRegistry;
+
     }
 
-/*    @Bean
-    public MeterRegistry meterRegistry() {
-        MeterRegistry meterRegistry = Metrics.globalRegistry;
-        logger.info("MDC get value:{}", MDC.get("TraceId"));
-        meterRegistry.config().commonTags("TraceId", MDC.get("TraceId"));
-        return meterRegistry;
-    }*/
+
+    @Override
+    public Mono<Void> handle(ServerHttpRequest request, ServerHttpResponse response) {
+        HttpHeaders headers = request.getHeaders();
+        String traceId = "TraceId";
+        Map<String, Object> attributes =  new HashMap<>();
+        for(String key : headers.keySet())
+            attributes.put(key,headers.getFirst(key));
+
+        Observation observation = ServerHttpObservationDocumentation.HTTP_REACTIVE_SERVER_REQUESTS
+                .observation(observationRegistry)
+                .start();
+        attributes.put(traceId, attributes.getOrDefault(traceId, UUID.randomUUID()));
+        attributes.put(Observation.class.getName(), observation);
+        ServerRequestObservationContext context = new ServerRequestObservationContext(request, response, attributes);
+        try (Observation.Scope scope = observation.openScope()) {
+
+        }
+        catch (Exception exception) {
+            observation.error(exception);
+            throw exception;
+        }
+        finally {
+            observation.stop();
+        }
+
+        return null;
+    }
 }
+
--- src/main/java/org/example/controller/SampleController.java
+++ src/main/java/org/example/controller/SampleController.java
@@ -2,11 +2,13 @@ package org.example.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 
-@RestController("/sampleMapping")
+@RestController
+@RequestMapping("/sampleMapping")
 public class SampleController {
 
     @GetMapping
